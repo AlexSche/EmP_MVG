4248416d56e47fa968fca097c74771367d97ab97
Mark_225 <mark@mark225.de>
2023-12-23 00:33:02 +0100

    Clean up PlayerController code and add comments

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -16,47 +16,65 @@ public class PlayerController : MonoBehaviour
     public float terminalVelocity = -10f;
     public float jumpForce = 5f;
     public float mouseSensitivity = 0.8f;
+    
     // Start is called before the first frame update
     void Start()
     {
+        //Create a control scheme instance and register listeners
         _controls = new EmPControls();
         _controls.Enable();
         _controls.Ingame.Camera.performed += OnCameraMove;
         _controls.Ingame.Jump.performed += OnJump;
         _characterController = GetComponent<CharacterController>();
+        //Hide and lock the cursor
         Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Read current movement input (already normalized) and transform it to world space
         Vector2 movement = _controls.Ingame.Move.ReadValue<Vector2>();
         Vector3 move = new Vector3(movement.x, 0, movement.y);
         move = transform.TransformDirection(move);
+        
+        //Move the character controller horizontally accounting for Time.deltaTime
         _characterController.Move(move * (Time.deltaTime * 5));
-        _yVelocity += gravity * Time.deltaTime;
+        
+        //Apply gravity to vertical velocity if character is not grounded
+        if(!_characterController.isGrounded)
+            _yVelocity += gravity * Time.deltaTime;
+        
+        //Clamp to terminal velocity
         if (_yVelocity < terminalVelocity)
-        {
             _yVelocity = terminalVelocity;
-        }
 
-        if (_characterController.isGrounded && _yVelocity < 0)
-        {
+        //Reset vertical velocity when grounded
+        if (_yVelocity < 0 && _characterController.isGrounded)
             _yVelocity = 0;
-        }
+        
+        //Apply vertical velocity to character controller accounting for Time.deltaTime
         _characterController.Move(new Vector3(0, _yVelocity, 0) * Time.deltaTime);
     }
 
+    //Every frame while the mouse is moved
     void OnCameraMove(InputAction.CallbackContext context)
     {
+        //Get Mouse movement (already accounts for Time.deltaTime)
         Vector2 cameraMovement = context.ReadValue<Vector2>();
+        
+        //Rotate the entire player object on the y axis and the camera on the x axis
         transform.Rotate(Vector3.up, cameraMovement.x * mouseSensitivity);
         cameraTransform.Rotate(Vector3.right, -cameraMovement.y * mouseSensitivity);
+        
+        //Clamp the camera rotation so it can't go upside down
         cameraTransform.localEulerAngles = new Vector3(cameraTransform.localEulerAngles.x, 0, 0);
     }
     
+    //Whenever jump is pressed
     void OnJump(InputAction.CallbackContext context)
     {
+        //This should only allow jumping when the character is grounded but doesn't work 100% of the time for some reason
         if (_characterController.isGrounded)
         {
             _yVelocity = jumpForce;
