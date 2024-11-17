51cb93e685d6662b12fb7e127cb5fbb8f52aafcf
AlexSche <Alexander.S_97@gmx.de>
2024-11-17 12:15:41 +0100

    connect start, gravitation and second level

--- Assets/Scripts/SceneSwitcher.cs
+++ Assets/Scripts/SceneSwitcher.cs
@@ -6,9 +6,26 @@ using UnityEngine.SceneManagement;
 public class SceneSwitcher : MonoBehaviour
 {
     public int sceneIndex;
+    public Collider triggerCollider;
+    private Collider playerCollider;
+    void Start()
+    {
+        var _playerController = FindFirstObjectByType<PlayerController>(FindObjectsInactive.Include);
+        playerCollider = _playerController.GetComponent<Collider>();
+    }
+
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(sceneIndex);
+            Debug.Log("Loading next Scene");
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (triggerCollider.bounds.Intersects(playerCollider.bounds))
         {
             SceneManager.LoadScene(sceneIndex);
             Debug.Log("Loading next Scene");
